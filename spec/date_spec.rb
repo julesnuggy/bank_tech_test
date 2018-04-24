b6915c6f8f3cd8cc8cbab9a44638d6da22de3aff
julesnuggy <julesnuggy@gmail.com>
2018-04-24 17:19:17 +0100

    Wrote and passed test for Date class

--- spec/date_spec.rb
+++ spec/date_spec.rb
@@ -1,10 +1,11 @@
 require_relative '../lib/date.rb'
 
 describe Date do
-  let(:fake_date) { double('date', date: '24/04/2018')}
-  subject(:date) { described_class.new(fake_date)}
+  let(:fake_date) { double('fake_date', date: Time.new(2018, 04, 24))}
+  subject(:date_instance) { described_class.new(fake_date.date)}
 
   it 'should return the date in dd/mm/yyyy format' do
-    expect(date).to equal('24/04/2018')
+    allow(fake_date).to receive(:strftime)
+    expect(date_instance.date).to eq('24/04/2018')
   end
 end
